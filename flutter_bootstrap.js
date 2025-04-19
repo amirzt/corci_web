3e04b76a0451966ffe96651d89f8befa73d32840
amirhossein zinati <amirhosseinzt78@gmail.com>
2025-04-19 09:25:21 +0330

    removed wasm

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3876692313"
+    serviceWorkerVersion: "4052017684"
   }
 });
